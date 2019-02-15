0f7b0f66e5a3adbbcb45e52bf2f4a1156a9c578b
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-02-15 11:59:54 +0100

    binder

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,7 @@
+c = get_config()
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+c.NotebookApp.password = "sha1:aedac97438e1:32a11318e5de7ede0e4bb98db3f97b7d7e38b476"
+c.NotebookApp.notebook_dir = "/opt/app/data"
+
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/bin/docker run -d -p 2036:9999 -v $(pwd)/books:/jupyter tschm/presentation:v1.1
+docker-compose run -p "9015:8888" web

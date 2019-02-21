a2ca1c72b8ffd623f3ddb7c67754a1c6c74644ba
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2019-02-21 16:10:40 +0100

    binder

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,6 +2,7 @@ c = get_config()
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
-c.NotebookApp.password = "sha1:aedac97438e1:32a11318e5de7ede0e4bb98db3f97b7d7e38b476"
-c.NotebookApp.notebook_dir = "/opt/app/data"
+c.NotebookApp.password = ""
+c.NotebookApp.notebook_dir = "/home/jovyan/work"
+c.NotebookApp.token = ""
 
--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo "http://localhost:9015"
 docker-compose run -p "9015:8888" web
